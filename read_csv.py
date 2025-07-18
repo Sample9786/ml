b3fae55b758baa3415b944d95025670aaf86b9df
Sample9786 <samplegit33@gmail.com>
2025-07-18 11:45:56 +0530

    Update read_csv.py desc

--- read_csv.py
+++ read_csv.py
@@ -6,6 +6,9 @@ print(df.head())
 print('Units Format in Rupees :\n')
 print(df[df['UNITS']=='rupees'])
 
+desc=df.describe()
+print(desc)
+
 print('Period before 2000s\n')
 new=df[df['Period']<=2000]
 
